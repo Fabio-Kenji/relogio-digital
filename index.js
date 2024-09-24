71743994ead3070576358d06949daacdb3371ca4
Fabio Kenji <fabiokenji110@gmail.com>
2024-09-23 23:31:17 -0300

    Modo de 12 horas

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function updateClock() {
 
     const agora = new Date()
-    const horas = agora.getHours().toString().padStart(2, 0)
+    let horas = agora.getHours()
+    const meridiem = horas >= 12 ? "PM" : "AM"
+    horas = horas % 12 || 12
+    horas = horas.toString().padStart(2, 0)
     const minutos = agora.getMinutes().toString().padStart(2, 0)
     const segundos = agora.getSeconds().toString().padStart(2, 0)
-    const timeString = `${horas}:${minutos}:${segundos}`
+    const timeString = `${horas}:${minutos}:${segundos} ${meridiem}`
     document.getElementById("clock").textContent = timeString
 }
 
