d3c6b2b9f40bf9f1a43101f7ced7d614a83441b9
Fabio Kenji <fabiokenji110@gmail.com>
2024-09-24 00:34:37 -0300

    Adicionado um botão para trocar de modo de 12 ou 24 horas

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
+let modo = true
+
 function updateClock() {
 
     const agora = new Date()
-    let horas = agora.getHours()
+    const horas = agora.getHours().toString().padStart(2, 0)
     const meridiem = horas >= 12 ? "PM" : "AM"
-    horas = horas % 12 || 12
-    horas = horas.toString().padStart(2, 0)
+    const horas12 = (horas % 12).toString().padStart(2, 0)
     const minutos = agora.getMinutes().toString().padStart(2, 0)
     const segundos = agora.getSeconds().toString().padStart(2, 0)
-    const timeString = `${horas}:${minutos}:${segundos} ${meridiem}`
-    document.getElementById("clock").textContent = timeString
+    const timeString = `${horas}:${minutos}:${segundos}`
+    const timeString12 = `${horas12}:${minutos}:${segundos} ${meridiem}`
+    if (modo) {
+        document.getElementById("clock").textContent = timeString
+    } else {
+        document.getElementById("clock").textContent = timeString12
+    }
+    
+}
+
+function trocaModo() {
+    if (modo) {
+        modo = false
+    } else {
+        modo = true
+    }
 }
 
 updateClock()
